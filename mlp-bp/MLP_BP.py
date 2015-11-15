7e5536efb95916a4810e5372c575acb99d4c3b58
Nitish Reddy Koripalli <nitish.k.reddy@gmail.com>
2015-11-15 21:12:46 +0100

    moved main to separate file

--- mlp-bp/MLP_BP.py
+++ mlp-bp/MLP_BP.py
@@ -233,33 +233,3 @@ class MLP_BP:
                 print "Test Input       :", input_.T
                 print "Activation       :", self.layer_stats_list[-1].A
                 print "Output           :", self.layer_stats_list[-1].Y
-                
-
-### TEST AREA ###
-# SETUP NETWORK
-if __name__ == '__main__':
-    train_data = np.genfromtxt("train_XOR.txt", delimiter=",", skip_header=1)
-    test_data = np.genfromtxt("train_XOR.txt", delimiter=",", skip_header=1)
-    
-    mlp_bp = MLP_BP(network_design=[2,2,1],
-              initial_weights=MLP_BP.WEIGHTS_RANDOM,
-              bias=True,
-              learning_rate=1,
-              train_type=MLP_BP.TRAIN_REGRESSION,
-              output_type=MLP_BP.OUTPUT_CLASSIFICATION,
-              activation_alpha=1)
-    
-    # Training
-    TRAIN_ITERATIONS = 1000
-    
-    for i in range(TRAIN_ITERATIONS):
-        train_data_ = train_data.copy()
-        np.random.shuffle(train_data_)
-        train_inputs_ = train_data_[:,:2]
-        train_targets_ = train_data_[:,2][:,np.newaxis]
-        
-        mlp_bp.train(train_inputs_, train_targets_, DEBUG=False)
-    
-    # Testing
-    test_input_ = test_data[:,:2]
-    mlp_bp.test(test_input_, DEBUG=True)
